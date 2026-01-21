c6226636dc24583eeee383428bc733006f4f494a
PrA-GYaN <pragyan1516@gmail.com>
2026-01-21 21:14:20 +0545

    fixed

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiBearerAuth, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import {
   CreateUserDto,
