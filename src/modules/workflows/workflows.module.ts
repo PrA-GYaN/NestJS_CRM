c1bafe9aa20bc9e2f404d4a6496067a58c419e5d
PrA-GYaN <pragyan1516@gmail.com>
2026-04-19 11:18:17 +0545

    New Apis

--- src/modules/workflows/workflows.module.ts
+++ src/modules/workflows/workflows.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { WorkflowsController } from './workflows.controller';
 import { WorkflowsService } from './workflows.service';
+import { WorkflowVersioningController } from './workflow-versioning.controller';
+import { WorkflowVersioningService } from './workflow-versioning.service';
+import { WorkflowMigrationService } from './workflow-migration.service';
+import { WorkflowAnalyticsService } from './workflow-analytics.service';
 import { TenantModule } from '../../common/tenant/tenant.module';
 
 @Module({
   imports: [TenantModule],
-  controllers: [WorkflowsController],
-  providers: [WorkflowsService],
-  exports: [WorkflowsService],
+  controllers: [WorkflowsController, WorkflowVersioningController],
+  providers: [
+    WorkflowsService,
+    WorkflowVersioningService,
+    WorkflowMigrationService,
+    WorkflowAnalyticsService,
+  ],
+  exports: [
+    WorkflowsService,
+    WorkflowVersioningService,
+    WorkflowMigrationService,
+    WorkflowAnalyticsService,
+  ],
 })
 export class WorkflowsModule {}
