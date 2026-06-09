f0ef8ba9e2c0e072b3091c0ba29753512e7b201b
PrA-GYaN <pragyan1516@gmail.com>
2026-06-09 17:00:43 +0545

    filesize issue fixed

--- src/modules/files/files.module.ts
+++ src/modules/files/files.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { TenantModule } from '../../common/tenant/tenant.module';
 
 @Module({
-  imports: [TenantModule],
+  imports: [
+    TenantModule,
+    MulterModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        limits: {
+          fileSize: configService.get('MAX_FILE_SIZE') || 10 * 1024 * 1024,
+        },
+      }),
+    }),
+  ],
   controllers: [FilesController],
   providers: [FilesService],
   exports: [FilesService],
